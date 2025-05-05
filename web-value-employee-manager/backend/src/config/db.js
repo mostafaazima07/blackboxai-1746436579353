913dabf5dfcd8369750c9b832dc477ebf354f36b
mostafaazima07 <info@thewebvalue.com>
2025-05-05 13:57:55 +0300

    Add web-value-employee-manager/backend/src/config/db.js 1746442676014

---
+++ web-value-employee-manager/backend/src/config/db.js
@@ -0,0 +1,23 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  logging: process.env.NODE_ENV === 'development' ? console.log : false,
+  dialectOptions: {
+    ssl: process.env.NODE_ENV === 'production' ? {
+      require: true,
+      rejectUnauthorized: false
+    } : false
+  },
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+});
+
+module.exports = {
+  sequelize,
+  Sequelize
+};
